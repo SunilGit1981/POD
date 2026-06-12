896f15cc24151f5ea9402b903d29347e2b2be853
ashwi <sunil05271981@gmail.com>
2026-06-12 16:23:18 +0530

    Feat: Autonomous agent run for login.html

--- validate_login_html.py
+++ validate_login_html.py
@@ -1,41 +1,45 @@
 import unittest
 from bs4 import BeautifulSoup
+import os
 
-class TestLoginHtml(unittest.TestCase):
+class TestLoginPage(unittest.TestCase):
 
     def setUp(self):
         with open('login.html', 'r') as file:
-            self.html_content = file.read()
-        self.soup = BeautifulSoup(self.html_content, 'html.parser')
+            self.html = file.read()
 
-    def test_gmail_auth_button(self):
-        gmail_auth_button = self.soup.find('button', id='gmail-auth-button')
-        self.assertIsNotNone(gmail_auth_button)
-        self.assertEqual(gmail_auth_button.text, 'Sign in with Gmail')
+    def test_login_page_structure(self):
+        soup = BeautifulSoup(self.html, 'html.parser')
+        self.assertIsNotNone(soup.find('div', class_='login-container'))
+        self.assertIsNotNone(soup.find('div', class_='login-header'))
+        self.assertIsNotNone(soup.find('form', class_='login-form'))
+        self.assertIsNotNone(soup.find('input', id='userid'))
+        self.assertIsNotNone(soup.find('input', id='password'))
+        self.assertIsNotNone(soup.find('button', class_='login-button'))
 
-    def test_gmail_auth_script(self):
-        script_tags = self.soup.find_all('script')
-        gmail_auth_script = None
-        for script in script_tags:
-            if 'gmailAuthButton' in str(script):
-                gmail_auth_script = script
-                break
-        self.assertIsNotNone(gmail_auth_script)
+    def test_login_form(self):
+        soup = BeautifulSoup(self.html, 'html.parser')
+        form = soup.find('form', class_='login-form')
+        self.assertIsNotNone(form)
+        self.assertEqual(form.get('id'), 'login-form')
 
-    def test_gmail_auth_container(self):
-        gmail_auth_container = self.soup.find('div', id='gmail-auth-container')
-        self.assertIsNotNone(gmail_auth_container)
+    def test_login_form_fields(self):
+        soup = BeautifulSoup(self.html, 'html.parser')
+        form = soup.find('form', class_='login-form')
+        self.assertIsNotNone(form.find('input', id='userid'))
+        self.assertIsNotNone(form.find('input', id='password'))
+        self.assertIsNotNone(form.find('button', class_='login-button'))
 
-    def test_gmail_auth_script_content(self):
-        script_tags = self.soup.find_all('script')
-        gmail_auth_script = None
-        for script in script_tags:
-            if 'gmailAuthButton' in str(script):
-                gmail_auth_script = script
-                break
-        self.assertIsNotNone(gmail_auth_script)
-        self.assertIn('https://accounts.google.com/o/oauth2/v2/auth', str(gmail_auth_script))
-        self.assertIn('https://openidconnect.googleapis.com/v1/userinfo', str(gmail_auth_script))
+    def test_login_button(self):
+        soup = BeautifulSoup(self.html, 'html.parser')
+        button = soup.find('button', class_='login-button')
+        self.assertIsNotNone(button)
+        self.assertEqual(button.get('type'), 'submit')
+
+    def test_script_tag(self):
+        soup = BeautifulSoup(self.html, 'html.parser')
+        script = soup.find('script')
+        self.assertIsNotNone(script)
 
 if __name__ == '__main__':
     unittest.main()
