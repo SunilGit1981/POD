4f5afe67a431c22caac0f7f1ca1cb3d2daf93926
ashwi <sunil05271981@gmail.com>
2026-06-12 16:35:54 +0530

    Enhance: Automated update cycle for login.html

--- validate_login_html.py
+++ validate_login_html.py
@@ -2,44 +2,60 @@ import unittest
 from bs4 import BeautifulSoup
 import os
 
-class TestLoginPage(unittest.TestCase):
+class TestLoginHtml(unittest.TestCase):
 
     def setUp(self):
         with open('login.html', 'r') as file:
             self.html = file.read()
 
-    def test_login_page_structure(self):
-        soup = BeautifulSoup(self.html, 'html.parser')
-        self.assertIsNotNone(soup.find('div', class_='login-container'))
-        self.assertIsNotNone(soup.find('div', class_='login-header'))
-        self.assertIsNotNone(soup.find('form', class_='login-form'))
-        self.assertIsNotNone(soup.find('input', id='userid'))
-        self.assertIsNotNone(soup.find('input', id='password'))
-        self.assertIsNotNone(soup.find('button', class_='login-button'))
+    def test_doctype(self):
+        self.assertIn('<!DOCTYPE html>', self.html)
 
-    def test_login_form(self):
-        soup = BeautifulSoup(self.html, 'html.parser')
-        form = soup.find('form', class_='login-form')
-        self.assertIsNotNone(form)
-        self.assertEqual(form.get('id'), 'login-form')
+    def test_html_tag(self):
+        self.assertIn('<html lang="en">', self.html)
 
-    def test_login_form_fields(self):
-        soup = BeautifulSoup(self.html, 'html.parser')
-        form = soup.find('form', class_='login-form')
-        self.assertIsNotNone(form.find('input', id='userid'))
-        self.assertIsNotNone(form.find('input', id='password'))
-        self.assertIsNotNone(form.find('button', class_='login-button'))
+    def test_head_tag(self):
+        self.assertIn('<head>', self.html)
+
+    def test_title_tag(self):
+        self.assertIn('<title>Login Page</title>', self.html)
+
+    def test_style_tag(self):
+        self.assertIn('<style>', self.html)
+
+    def test_body_tag(self):
+        self.assertIn('<body>', self.html)
+
+    def test_container_div(self):
+        self.assertIn('<div class="container">', self.html)
+
+    def test_form_tag(self):
+        self.assertIn('<form>', self.html)
+
+    def test_userid_input(self):
+        self.assertIn('<input type="text" id="userid" name="userid" required>', self.html)
+
+    def test_password_input(self):
+        self.assertIn('<input type="password" id="password" name="password" required>', self.html)
 
     def test_login_button(self):
-        soup = BeautifulSoup(self.html, 'html.parser')
-        button = soup.find('button', class_='login-button')
-        self.assertIsNotNone(button)
-        self.assertEqual(button.get('type'), 'submit')
+        self.assertIn('<button class="btn" type="submit">Login</button>', self.html)
 
-    def test_script_tag(self):
+    def test_footer_tag(self):
+        self.assertIn('<footer>', self.html)
+
+    def test_copyright_statement(self):
+        self.assertIn('&copy; 2024 Login Page. All rights reserved.', self.html)
+
+    def test_responsive_css(self):
         soup = BeautifulSoup(self.html, 'html.parser')
-        script = soup.find('script')
-        self.assertIsNotNone(script)
+        self.assertIsNotNone(soup.find('meta', attrs={'name': 'viewport'}))
+
+    def test_dark_layout(self):
+        soup = BeautifulSoup(self.html, 'html.parser')
+        style_tag = soup.find('style')
+        self.assertIsNotNone(style_tag)
+        self.assertIn('background-color: #2f2f2f', str(style_tag))
 
 if __name__ == '__main__':
     unittest.main()
